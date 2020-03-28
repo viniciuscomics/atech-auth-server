74f447c13383bcc758004a6ddd5a8561764c6ee2
Vinicius Costa <42116742+viniciuscomics@users.noreply.github.com>
2020-03-28 17:36:07 -0300

    First auth-server project upload.

---
+++ src/main/java/com/atech/AtechAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.atech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtechAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtechAuthServerApplication.class, args);
+	}
+
+}
